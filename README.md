64108cfcce4402571b793ad4a2f6df933a8c9c81
bilal omar <bilalomarfawzi@gmail.com>
2025-12-28 17:08:33 +0300

    v1.1

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ we will use ArrayList as three nested. ex: ArrayList<ArrayList<ArrayList<String>
 4- Participant ID   "user will enter"
 
 Menu Methods:
- Menu: it will have 9 switch cases + default
-1- it will store the Event Categories.
-2- we will go to another method displaying the available categories the choosing one of them after that display event names and choosing one of them then prompt the user to add Name-ID.
+ Menu: it will have 8 switch cases + default
+1- A: it will store the Event Categories.
+   B: we will go to another method displaying the available categories the choosing one of them after that display event names and choosing one of them then prompt the user to add Name-ID.
 3- we would prompt the user to enter the Event Name/Category + Participant Name/ID
 4- we will display participants and event names per category (nested loops).
 5- display total number of participants in each category and events.
--- assignment.java
+++ assignment.java
@@ -1,5 +1,74 @@
-package Assignment;
+//package Assignment;
+
+import java.util.*;
 
 public class assignment {
-    
+    static Scanner input = new Scanner(System.in);
+    static int choice;
+
+    public static void main(String[] args) {
+        do {
+            menu();
+            switch (choice) {
+
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+
+                case 8:
+
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+
+        } while (choice != 0);
+    }
+
+    public static void menu() {
+        System.out.println("-------------------------");
+        System.out.println("1. Choose an event category");
+        System.out.println("2. Choose an event");
+        System.out.println("3. Remove a participant");
+        System.out.println("4. Display al registered participant");
+        System.out.println("5. Display total number of registrations");
+        System.out.println("6. Search for a participant");
+        System.out.println("7. Display recent registrations");
+        System.out.println("8. Generate a summary");
+        System.out.println("Enter (0) to exit ");
+        System.out.println("-------------------------");
+        try {
+            System.out.print("Enter a choice ");
+            choice = input.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Error: That's not a valid Choice. Try again.");
+            input.next();
+            choice = -1;
+        }
+    }
 }
